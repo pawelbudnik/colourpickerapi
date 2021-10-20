767637e1327067827e124569047a00e957406781
PiskoOo <piskooooooo@gmail.com>
2021-10-21 00:25:45 +0200

    last changes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,28 +1,32 @@
 import React from "react";
+import './App.css';
+
 const apiUri = "http://localhost:8080/api/v1/colourpicker/";
 
 class Colours extends React.Component {
   render() {
-      return (
-          <table>
-              <thead>
-                  <tr>
-                      <th>Colour</th>
-                      <th>Hex</th>
-                      <th>RGB</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {this.props.colours && this.props.colours.map(colour => {
-                      return <tr>
-                          <td>{colour.colour}</td>
-                          <td>{colour.hex}</td>
-                          <td>{colour.rgb}</td>
-                      </tr>
-                  })}
-              </tbody>
-          </table>
-      );
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Colour</th>
+            <th>Hex</th>
+            <th>RGB</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.colours && this.props.colours.map(colour => {
+            return <tr>
+              <td style={{ color:colour.rgb }}>{colour.colour}</td>
+              <td>{colour.hex}</td>
+              <td>{colour.rgb}</td>
+              <td>{colour._id}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    );
   }
 }
 
@@ -46,16 +50,17 @@ class App extends React.Component {
   componentDidMount() {
     // read all entities
     fetch(apiUri, {
-        "method": "GET",
+      "method": "GET",
     })
-    .then(response => response.json())
-    .then(response => {
+      .then(response => response.json())
+      .then(response => {
         this.setState({
-        colours: response
+          colours: response
         })
-    })
-    .catch(err => { console.log(err); 
-    });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   create(e) {
@@ -72,34 +77,50 @@ class App extends React.Component {
         rgb: this.state.rgb
       })
     })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response)
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err);
+      });
     e.preventDefault();
   }
 
   update(e) {
     // update entity - PUT
-    e.preventDefault();
-
+    fetch(`${apiUri}${this.state._id}`, {
+      "method": "PUT",
+      "headers": {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      "body": JSON.stringify({
+        colour: this.state.colour,
+        hex: this.state.hex,
+        rgb: this.state.rgb,
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => { console.log(err); });
   }
 
   delete(e) {
     // deletes entities
-    fetch(`http://localhost:8080/api/v1/colourpicker/${this.state.colours[this.state.colours.length - 1]._id}`, {
+    console.log(this.state._id)
+    fetch(`${apiUri}${this.state._id}`, {
       "method": "DELETE",
     })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   handleChange(changeObject) {
@@ -108,64 +129,76 @@ class App extends React.Component {
 
   render() {
     return (
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-8">
-              <h1 className="display-4 text-center">Add-Update-Delete Colour</h1>
-              <form className="d-flex flex-column">
-                <label htmlFor="colour">
-                  Colour:
-                  <input
-                    name="colour"
-                    id="colour"
-                    type="text"
-                    className="form-control"
-                    value={this.state.colour}
-                    onChange={(e) => this.handleChange({ colour: e.target.value })}
-                    required
-                    />
-                </label>
-                <label htmlFor="hex">
-                  Colour Hex:
-                  <input
-                    name="hex"
-                    id="hex"
-                    type="text"
-                    className="form-control"
-                    value={this.state.hex}
-                    onChange={(e) => this.handleChange({ hex: e.target.value })}
-                    required
-                    />
-                </label>
-                <label htmlFor="rgb">
-                  Colour RGB:
-                  <input
-                    name="rgb"
-                    id="rgb"
-                    type="text"
-                    className="form-control"
-                    value={this.state.rgb}
-                    onChange={(e) => this.handleChange({ rgb: e.target.value })}
-                    />
-                </label>
-                <br/>
-                <button className="btn btn-primary" type='button' onClick={(e) => this.create(e)}>
-                  Add
-                </button>
-                <br/>
-                <button className="btn btn-info" type='button' onClick={(e) => this.update(e)}>
-                    Update
-                </button>
-                <br/>
-                <button className="btn btn-danger" type='button' onClick={(e) => this.delete(e)}>
-                    Delete
-                </button>
-                <br/>
-              </form>
-              <Colours colours={this.state.colours} />
-            </div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h1 className="display-4 text-center">Add-Update-Delete Colour</h1>
+            <form className="d-flex flex-column">
+              <label htmlFor="colour">
+                Colour:
+                <input
+                  name="colour"
+                  id="colour"
+                  type="text"
+                  className="form-control"
+                  value={this.state.colour}
+                  onChange={(e) => this.handleChange({ colour: e.target.value })}
+                  required
+                />
+              </label>
+              <label htmlFor="hex">
+                Colour Hex:
+                <input
+                  name="hex"
+                  id="hex"
+                  type="text"
+                  className="form-control"
+                  value={this.state.hex}
+                  onChange={(e) => this.handleChange({ hex: e.target.value })}
+                  required
+                />
+              </label>
+              <label htmlFor="rgb">
+                Colour RGB:
+                <input
+                  name="rgb"
+                  id="rgb"
+                  type="text"
+                  className="form-control"
+                  value={this.state.rgb}
+                  onChange={(e) => this.handleChange({ rgb: e.target.value })}
+                />
+              </label>
+              <br />
+              <label htmlFor="id">
+                Colour ID:
+                <input
+                  name="id"
+                  id="id"
+                  type="text"
+                  className="form-control"
+                  value={this.state._id}
+                  onChange={(e) => this.handleChange({ _id: e.target.value })}
+                />
+              </label>
+              <br />
+              <button className="btn btn-primary" type='button' onClick={(e) => this.create(e)}>
+                Add Colour
+              </button>
+              <br />
+              <button className="btn btn-info" type='button' onClick={(e) => this.update(e)}>
+                Update with ID
+              </button>
+              <br />
+              <button className="btn btn-danger" type='button' onClick={(e) => this.delete(e)}>
+                Delete with ID
+              </button>
+              <br />
+            </form>
+            <Colours colours={this.state.colours} />
           </div>
         </div>
+      </div>
     );
   }
 }
